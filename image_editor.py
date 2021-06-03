d098066ae8ab5d97a07d50a3279505966deb2317
rahul khattri <rahulkhattri14@gmail.com>
2021-06-03 21:58:41 +0530

    ..

--- image_editor.py
+++ image_editor.py
@@ -7,7 +7,7 @@ def app():
         """
         <style>
         .reportview-container {
-            background: url("https://images.unsplash.com/photo-1516724562728-afc824a36e84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1351&q=80") no-repeat center fixed;
+            background: url("https://images.unsplash.com/photo-1542124292-60272943a355?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80") no-repeat center fixed;
             background-size: cover;
         }
     .sidebar .sidebar-content {
